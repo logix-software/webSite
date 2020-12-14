b8c67c5aa288ab571cc632f0f9550f857afe61c5
Luca Spezzano <spezzanoluca@gmail.com>
2020-12-14 18:57:13 +0100

    added layout to legal note page

--- src/pages/Note-legali.vue
+++ src/pages/Note-legali.vue
@@ -1,9 +1,18 @@
 <template>
-  <div class="container py-5">
-    <div v-for="text in $page.texts.edges" :key="text.id">
-      <h1 class="mb-4">{{text.node.title}}</h1>
-      <div v-html="text.node.content" />
+  <div>
+    <div class="container py-5">
+      <div class="mb-4">
+         <g-link to="/">
+            <g-image src="~/assets/images/logo/logix-software-black.svg" class="header__logo" alt="logix-software" />
+          </g-link>
+      </div>
+
+      <div v-for="text in $page.texts.edges" :key="text.id">
+        <h1 class="mb-4">{{text.node.title}}</h1>
+        <div v-html="text.node.content" />
+      </div>
     </div>
+    <Footer />
   </div>
 </template>
 <page-query>
@@ -19,7 +28,12 @@ query {
 }
 </page-query>
 <script>
+import Footer from "~/components/Footer.vue";
+
 export default {
+  components: {
+    Footer
+  },
   metaInfo: {
     title: "Note Legali - Logix Software",
   },
