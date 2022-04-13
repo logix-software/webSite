dabc910641e36ae603c84889f989daf1af9e2e9d
Luca Spezzano <spezzanoluca@gmail.com>
2022-04-13 11:26:05 +0200

    Added iubenda cookies and removed custom cookies

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -3,31 +3,6 @@
     <Navbar />
     <slot />
     <Footer />
-    <div class="cookies py-4">
-      <div class="container">
-        <div class="row">
-          <div class="col-2 col-lg-1 d-flex align-items-center justify-content-between">
-            <div>
-              <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="cookie" class="svg-inline--fa fa-cookie" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M510.37 254.79l-12.08-76.26a132.493 132.493 0 0 0-37.16-72.95l-54.76-54.75c-19.73-19.72-45.18-32.7-72.71-37.05l-76.7-12.15c-27.51-4.36-55.69.11-80.52 12.76L107.32 49.6a132.25 132.25 0 0 0-57.79 57.8l-35.1 68.88a132.602 132.602 0 0 0-12.82 80.94l12.08 76.27a132.493 132.493 0 0 0 37.16 72.95l54.76 54.75a132.087 132.087 0 0 0 72.71 37.05l76.7 12.14c27.51 4.36 55.69-.11 80.52-12.75l69.12-35.21a132.302 132.302 0 0 0 57.79-57.8l35.1-68.87c12.71-24.96 17.2-53.3 12.82-80.96zM176 368c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32 32-14.33 32-32 32zm32-160c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32 32-14.33 32-32 32zm160 128c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32 32-14.33 32-32 32z"></path></svg>
-            </div>
-          </div>
-          <div class="col-10 col-lg-8 d-flex align-items-center">
-            <p class="mb-0">
-              Questo sito fa uso di cookie per migliorare l’esperienza di navigazione degli utenti e per raccogliere informazioni sull’utilizzo del sito stesso. Può conoscere i dettagli consultando la nostra privacy policy
-              <a
-                href="https://www.iubenda.com/privacy-policy/51005590"
-                target="_blank"
-                rel="noopener noreferrer"
-              >qui</a>. Selezionando accetta si acconsente all’uso dei cookie.
-            </p>
-          </div>
-          <div class="col-lg-3 d-flex align-items-center justify-content-center mt-4 mt-lg-0">
-            <g-link to="/cookies-not-accepted" class="btn btn-negative mr-3">Nega</g-link>
-            <button class="btn btn-primary" @click="hideCookies()">Accetta</button>
-          </div>
-        </div>
-      </div>
-    </div>
     <!-- Modal -->
     <div
       class="modal fade modalContact"
@@ -245,23 +220,7 @@ export default {
         .then(() => (_this.formSent = true))
         .catch(error => alert(error));
     },
-
-    checkCookies: function() {
-      if (document.cookie.match(/cookies=1/gi)) {
-        $(".cookies").hide();
-      }
-    },
-    hideCookies: function() {
-      document.cookie =
-        "cookies=1; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/;";
-      this.checkCookies();
-    }
   },
-  mounted() {
-    if (document.cookie.match(/cookies=1/gi)) {
-      $(".cookies").hide();
-    }
-  }
 };
 </script>
 <style lang="scss">
@@ -776,26 +735,7 @@ input:focus ~ .highlight {
     background: transparent;
   }
 }
-.cookies {
-  background-color: #000;
-  color: #fff;
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  z-index: 9999999;
-  .svg-inline--fa{
-    width: 36px;
-  }
-  p {
-    font-size: 12px;
-    a {
-      color: #fff;
-      font-weight: bold;
-      text-decoration: underline;
-    }
-  }
-}
+
 .text-green-contact{
   color: #36F2B9;
 }
